ae8edad97c164bbc1a87e483878da2792dbe8c57
jost3 <1427593@senati.pe>
2023-09-07 11:19:23 -0500

    agrego un boton de cerrar y diseño en la parte superior

--- sesion/login_J/Form1.Designer.cs
+++ sesion/login_J/Form1.Designer.cs
@@ -29,30 +29,61 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
+            btnCerrar = new Button();
+            label1 = new Label();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
             // 
+            panel1.BackColor = Color.DodgerBlue;
+            panel1.Controls.Add(btnCerrar);
             panel1.Dock = DockStyle.Top;
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
             panel1.Size = new Size(800, 43);
             panel1.TabIndex = 0;
             // 
+            // btnCerrar
+            // 
+            btnCerrar.BackColor = Color.Red;
+            btnCerrar.ForeColor = Color.White;
+            btnCerrar.Location = new Point(758, 12);
+            btnCerrar.Name = "btnCerrar";
+            btnCerrar.Size = new Size(30, 23);
+            btnCerrar.TabIndex = 1;
+            btnCerrar.Text = "X";
+            btnCerrar.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Snap ITC", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(285, 110);
+            label1.Name = "label1";
+            label1.Size = new Size(176, 42);
+            label1.TabIndex = 1;
+            label1.Text = "LOGION";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 647);
+            Controls.Add(label1);
             Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "Form1";
             Text = "Form1";
+            panel1.ResumeLayout(false);
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Panel panel1;
+        private Button btnCerrar;
+        private Label label1;
     }
 }
